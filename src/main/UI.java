56310416bba2fe994e85d0abe4fff8c41acbd9d0
yeojoey <yeojoey@gmail.com>
2015-09-24 10:41:43 +0800

    Add UI class

---
+++ src/main/UI.java
@@ -0,0 +1,16 @@
+package main;
+
+public class UI {
+
+	private static final String MESSAGE_WELCOME = "Welcome to Magical!";
+	private static final String MESSAGE_ERROR = "An error has occurred.";
+	
+	public static void displayWelcomeMessage() {
+		System.out.println(MESSAGE_WELCOME);
+	}
+	
+	public static void displayErrorMessage() {
+		System.out.println(MESSAGE_ERROR);
+	}
+	
+}
