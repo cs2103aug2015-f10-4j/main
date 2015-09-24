a2c99b02f14814dc7b02b5a639d66e283201bc4e
jtwjing <jtwjing@hotmail.com>
2015-09-24 11:30:20 +0800

    create isValidCmd and isValidArgs methods, create hasValidInput var
    1. isValidCmd checks for valid commands
    2. isValidArgs checks for valid arguments
    3. hasValidInputs is for checking if execute has been called at least
    once

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,7 +1,7 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 public class Parser {
 
@@ -11,12 +11,15 @@ public class Parser {
 	private static final String[] COMMANDS = {"add", "block", "confirm", "delete", 
 			"edit", "done", "show", "date", "undo", "search", "remind", "tag", 
 			"untag", "help", "exit"};
+	private static final String MESSAGE_EXECUTE_ERROR = "Parser has not been executed";
 	private static final String MESSAGE_INVALID_COMMAND = "Invalid command";
 	private static final String MESSAGE_INVALID_ARGS = "Invalid arguments";
+	private boolean hasValidInput = false;
 	
-	public void execute(String userInput){
+	public void execute(String userInput) throws Exception{
 		this.userInput = userInput;
 		splitInput(this.userInput);
+		this.hasValidInput = isValidCmd() && isValidArgs();
 	}
 	
 	private void splitInput (String input){
@@ -25,31 +28,66 @@ public class Parser {
 		this.args = toSplit[1];
 	}
 	
-	public String readCmd() throws Exception{
+	private boolean isValidCmd()throws Exception{
 		if(Arrays.asList(COMMANDS).contains(this.command)){
-			return this.command;
+			return true;
 		} else {
 			throw new Exception(MESSAGE_INVALID_COMMAND);
 		}
 	}
 	
+	private boolean isValidArgs(){
+		return true;
+		//switch()
+	}
+	
+	private void checkExecute() throws Exception{
+		if(!this.hasValidInput){
+			throw new Exception(MESSAGE_EXECUTE_ERROR);
+		}
+	}
+	
+	public String readCmd() throws Exception {
+		checkExecute();
+		return this.command;
+	}
+	
+
+	public Hashtable readArgs(){
+		
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		
+		//Correct
 		try {
-			Parser p = new Parser();
-			p.execute("Add task/test/this is a test/24092015/1000/1700/daily");
-			System.out.println(p.userInput);
-			System.out.println(p.command);
-			System.out.println(p.args);
-			System.out.println(p.readCmd());
+			System.out.println("------- TEST 1-------");
+			Parser p1 = new Parser();
+			p1.execute("Add task/test/this is a test/24092015/1000/1700/daily");
+			System.out.println(p1.userInput);
+			System.out.println(p1.command);
+			System.out.println(p1.args);
+			System.out.println(p1.readCmd());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		//Invalid input
 		try {
-			Parser p = new Parser();
-			p.execute("foobar task/test/this is a test/24092015/1000/1700/daily");
-			p.readCmd();
+			System.out.println("------- TEST 2-------");
+			Parser p2 = new Parser();
+			p2.execute("foobar task/test/this is a test/24092015/1000/1700/daily");
+			p2.readCmd();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		//No input
+		try {
+			System.out.println("------- TEST 3-------");
+			Parser p3 = new Parser();
+			p3.readCmd();
 		} catch (Exception e){
 			e.printStackTrace();
 		}
