26508a1f959a9edfe98331b70d4287b8d0ff9bcf
yeojoey <yeojoey@gmail.com>
2015-09-24 11:11:38 +0800

    Modify UI

--- src/main/UI.java
+++ src/main/UI.java
@@ -8,16 +8,22 @@ public class UI {
 	private static final String MESSAGE_COMMAND_PROMPT = "What would you like to do?";
 	private static final String MESSAGE_ERROR = "An error has occurred.";
 	
+	private static final String MESSAGE_TASK_ADDED = "Added task: %s";
+	private static final String MESSAGE_TASK_DELETED = "Deleted task: %s";
+	
+	private static final String DISPLAY_TASK_LONG = "";
+	
 	private static Scanner scanner = new Scanner(System.in);
 	
 	public static void start() {
 		displayWelcomeMessage();
-		while(true) {
-			showToUser(MESSAGE_COMMAND_PROMPT);
-			System.out.print("> ");
-			String userInput = scanner.nextLine();
-			showToUser("You said: " + userInput );
-		}
+	}
+	
+	private static String readInput() {
+		showToUser(MESSAGE_COMMAND_PROMPT);
+		System.out.print("> ");
+		String userInput = scanner.nextLine();
+		return userInput;
 	}
 	
 	private static void showToUser(String text) {
@@ -30,6 +36,10 @@ public class UI {
 	
 	private static void displayErrorMessage() {
 		showToUser(MESSAGE_ERROR);
+	}
+	
+	private static void displayTasks(ArrayList<Task> tasks) {
+		
 	}
 	
 }
