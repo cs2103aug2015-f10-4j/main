c336f8d48d343e67a91d53fdccead3bca7b99787
jtwjing <jtwjing@hotmail.com>
2015-09-24 12:31:35 +0800

    Some changes

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Hashtable;
 
 public class Parser {
@@ -14,21 +15,25 @@ public class Parser {
 	private static final String MESSAGE_EXECUTE_ERROR = "Parser has not been executed";
 	private static final String MESSAGE_INVALID_COMMAND = "Invalid command";
 	private static final String MESSAGE_INVALID_ARGS = "Invalid arguments";
+	private static final String MESSAGE_INVALID_NUM_INPUTS = "Invalid number of inputs";
 	private boolean hasValidInput = false;
 	
 	public void execute(String userInput) throws Exception{
 		this.userInput = userInput;
 		splitInput(this.userInput);
-		this.hasValidInput = isValidCmd() && isValidArgs();
+		//this.hasValidInput = isValidCmd() && isValidArgs();
+		this.hasValidInput = isValidInput(this.command);
 	}
 	
+	//Splits the input to command and args
 	private void splitInput (String input){
 		String[] toSplit = input.split(" ", 2);
 		this.command = toSplit[0].toLowerCase();
 		this.args = toSplit[1];
 	}
 	
-	private boolean isValidCmd()throws Exception{
+	//DONT NEED THIS ANYMORE
+	private boolean isValidCmd() throws Exception{
 		if(Arrays.asList(COMMANDS).contains(this.command)){
 			return true;
 		} else {
@@ -36,11 +41,50 @@ public class Parser {
 		}
 	}
 	
-	private boolean isValidArgs(){
-		return true;
-		//switch()
+	//checks if command and args are valid
+	private boolean isValidInput(String command) throws Exception{
+		switch(command){
+			case "add":
+				return checkAdd(this.args);
+			default:
+				throw new Exception(MESSAGE_INVALID_COMMAND);
+		}
 	}
 	
+	//checks if add command arguments are okay
+	private boolean checkAdd(String args) throws Exception {
+		int count = args.length() - args.replace("/", "").length();
+		
+		if(count != 6){
+			throw new Exception(MESSAGE_INVALID_NUM_INPUTS);
+		}
+		
+		String[] argsArray = args.split("/");
+		System.out.println(Arrays.toString(argsArray));
+		String type = argsArray[0];
+		String title = argsArray[1];
+		String dueDate = argsArray[3];
+		String startTime = argsArray[4];
+		String endTime = argsArray[5];
+		String recurrence;
+		checkAddType(type);
+		if(argsArray.length == 6){
+			recurrence = null;
+		} else {
+			recurrence = argsArray[6];
+		}
+		return true;
+	}
+
+	private void checkAddType(String type) throws Exception {
+		if(type.toLowerCase().equals("event") || type.toLowerCase().equals("task")){
+			
+		} else {
+			//Write the exception type next time
+			throw new Exception();
+		}
+	}
+
 	private void checkExecute() throws Exception{
 		if(!this.hasValidInput){
 			throw new Exception(MESSAGE_EXECUTE_ERROR);
@@ -53,9 +97,25 @@ public class Parser {
 	}
 	
 
-	public Hashtable readArgs(){
+	public Hashtable<String, String> readArgs(){
 		
-		return null;
+		Hashtable<String, String> argsTable = new Hashtable<String, String>();
+		String[] argsArray = args.split("/");
+		System.out.println(Arrays.toString(argsArray));
+		String type = argsArray[0];
+		argsTable.put("type", type);
+		String title = argsArray[1];;
+		argsTable.put("title", title);
+		String dueDate = argsArray[3];
+		argsTable.put("dueDate", dueDate);
+		String startTime = argsArray[4];
+		argsTable.put("startTime", startTime);
+		String endTime = argsArray[5];
+		argsTable.put("endTime", endTime);
+		String recurrence = argsArray[6];
+		argsTable.put("recurrence", recurrence);
+		return argsTable;
+		
 	}
 	
 	public static void main(String[] args) {
@@ -67,13 +127,15 @@ public class Parser {
 			p1.execute("Add task/test/this is a test/24092015/1000/1700/daily");
 			System.out.println(p1.userInput);
 			System.out.println(p1.command);
-			System.out.println(p1.args);
-			System.out.println(p1.readCmd());
+			System.out.println(p1.checkAdd(p1.args));
+			//System.out.println(p1.args);
+			//System.out.println(p1.readCmd());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		//Invalid input
+		/*
 		try {
 			System.out.println("------- TEST 2-------");
 			Parser p2 = new Parser();
@@ -82,8 +144,10 @@ public class Parser {
 		} catch (Exception e){
 			e.printStackTrace();
 		}
+		//*/
 		
 		//No input
+		/*
 		try {
 			System.out.println("------- TEST 3-------");
 			Parser p3 = new Parser();
@@ -91,5 +155,6 @@ public class Parser {
 		} catch (Exception e){
 			e.printStackTrace();
 		}
+		//*/
 	}
 }
