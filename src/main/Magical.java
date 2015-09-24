e48ee6ad6df4fd208d0394918eaa9a0061fc5b0a
Varun Patro <varun.kumar.patro@gmail.com>
2015-09-24 13:59:17 +0800

    create small working test case for add task

--- src/main/Magical.java
+++ src/main/Magical.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Magical {
+	private static final String CONFIG_STORAGE_FILENAME = "storage.txt"; 
+
 	private static final String MESSAGE_INVALID_CMD = "Invalid command given.";
 	
 	private static UI ui = new UI();
 	private static Parser parser = new Parser();
-	private static Storage storage = new Storage();
+	private static Storage storage = new Storage(CONFIG_STORAGE_FILENAME);
 	
 	public static void main(String args[]) {
 		try {
@@ -22,41 +26,68 @@ public class Magical {
 		}
 	}
 	
-	public static String executeCommand(String cmd, HashMap<String, String> args) {
+	public static String executeCommand(String cmd, HashMap<String, String> args) throws ParseException {
 		switch(cmd) {
 		case "add":
 			return add(args);
-		case "block":
-			return block(args);
-		case "confirm":
-			return confirm(args);
-		case "delete":
-			return delete(args);
-		case "edit":
-			return edit(args);
-		case "done":
-			return done(args);
-		case "show":
-			return show(args);
-		case "date":
-			return date(args);
-		case "undo":
-			return undo(args);
-		case "search":
-			return search(args);
-		case "remind":
-			return remind(args);
-		case "tag":
-			return tag(args);
-		case "untag":
-			return untag(args);
-		case "help":
-			return help();
+//		case "block":
+//			return block(args);
+//		case "confirm":
+//			return confirm(args);
+//		case "delete":
+//			return delete(args);
+//		case "edit":
+//			return edit(args);
+//		case "done":
+//			return done(args);
+//		case "show":
+//			return show(args);
+//		case "date":
+//			return date(args);
+//		case "undo":
+//			return undo(args);
+//		case "search":
+//			return search(args);
+//		case "remind":
+//			return remind(args);
+//		case "tag":
+//			return tag(args);
+//		case "untag":
+//			return untag(args);
+//		case "help":
+//			return help();
 		case "exit":
 			return exit();
 		default:
 			return MESSAGE_INVALID_CMD;
 		}		
+	}
+
+	private static String add(HashMap<String, String> args) throws ParseException {
+		Task task = new Task();
+		if (args.get("type").equals("task")) {
+			task.setType(Task.Type.TASK);
+		} else {
+			task.setType(Task.Type.EVENT);
+		}
+
+		task.setTitle(args.get("title"));
+		task.setDescription(args.get("description"));
+		task.setRecurrence(args.get("recurrence"));
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		task.setDueDate(dateFormat.parse(args.get("dueDate")));
+		task.setStartTime(Integer.parseInt(args.get("startTime")));
+		task.setEndTime(Integer.parseInt(args.get("endTime")));
+
+		storage.writeData(task);
+		return "task added";
+	}
+
+	private static String exit() {
+		ui.displayGoodbyeMessage();
+		System.exit(0);
+		return null;
 	}
 	
 }
