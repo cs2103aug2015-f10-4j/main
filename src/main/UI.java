2e46bd043695665acf6d91fb8d87a8938eda2bc2
yeojoey <yeojoey@gmail.com>
2015-09-30 21:42:46 +0800

    Add tags to displayTaskDetails

--- src/main/UI.java
+++ src/main/UI.java
@@ -62,8 +62,15 @@ public class UI {
 
 	public static void displayTaskDetails(Task task) {
 		displayHeader(task.getTitle());
+		Set<String> taskTags = task.getTags();
+		Iterator<String> iterator = taskTags.iterator();
+		String tagString = "";
+		for (int i = 0; i < taskTags.size() - 1; i++) {
+			tagString += iterator.next() + ", ";
+		}
+		tagString += iterator.next();
 		showToUser("Description: " + task.getDescription() +
-				"\r\nDue Date: " + makeShortDate(task.getDueDate()) + "\r\n");
+				"\r\nDue Date: " + makeShortDate(task.getDueDate()) + "\r\nTags: " + tagString);
 		
 	}
 	
