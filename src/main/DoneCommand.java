ad33ea554609a7a99ce9203dbe6834f1c12b5fcd
Varun Patro <varun.kumar.patro@gmail.com>
2015-09-30 21:49:20 +0800

    make done command functional

--- src/main/DoneCommand.java
+++ src/main/DoneCommand.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.io.IOException;
+import java.util.Set;
+
 public class DoneCommand extends Command{
 	private String taskID;
 	private String error = "";
+	private boolean hasExecuted = false;
+	private Task task;
 	
 	public DoneCommand(String args) throws Exception{
 		super(args);
@@ -37,14 +42,34 @@ public class DoneCommand extends Command{
 		return checkTaskID(this.taskID);
 	}
 	
-	@Override
-	public String execute(){
-		return null;
+	public String execute() {
+		Task task = Magical.ui.getLastTaskListDisplayed().get(taskID);
+		Set<String> tags = task.getTags();
+		tags.add("done");
+		task.setTags(tags);
+		try {
+			Magical.storage.updateTask(task);
+			hasExecuted = true;
+			return "task archived";
+		} catch (IOException e) {
+			return "unable to archive task";
+		}
 	}
 	
-	@Override
-	public String undo(){
-		return null;
+	public String undo() {
+		if (hasExecuted) {
+			Set<String> tags = task.getTags();
+			tags.remove("done");
+			task.setTags(tags);
+			try {
+				Magical.storage.updateTask(task);
+				return "task unarchived";
+			} catch (IOException e) {
+				return "unable to unarchive task";
+			}
+		} else {
+			return "cannot undo failed done command";
+		}
 	}
 }
 
