beb790f9d50a3dacdab002f93cf6db6cbebc78d7
jtwjing <jtwjing@hotmail.com>
2015-09-30 19:13:00 +0800

    create DoneCommand

---
+++ src/main/DoneCommand.java
@@ -0,0 +1,48 @@
+package main;
+
+public class DoneCommand extends Command{
+	private String taskID;
+	private String error = "";
+	
+	public DoneCommand(String args) throws Exception{
+		super(args);
+		
+		if(validNumArgs()){
+			taskID = args;
+			if(!validTaskID()){
+				error += "Task ID" + taskID + "\n";
+			}
+			if (!error.equals("")) {
+				throw new Exception("\n----- Invalid arguments ---- \n" + error);
+			}			
+		} else {
+			error += "Number of Arguments\n";
+			throw new Exception("\n----- Invalid arguments ---- \n" + error);
+		}
+	}
+	
+	private boolean checkCount() {
+		if (this.count != 1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean validNumArgs() {
+		return checkCount();
+	}
+	
+	public boolean validTaskID(){
+		return checkTaskID(this.taskID);
+	}
+	@Override
+	public String execute(){
+		return null;
+	}
+	@Override
+	public String undo(){
+		return null;
+	}
+}
+
