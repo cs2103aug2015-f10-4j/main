78fc1fc63aa78ac7c58020be6bccfddee0567ae3
Varun Patro <varun.kumar.patro@gmail.com>
2015-09-30 19:12:05 +0800

    implement execute and undo for Delete case

--- src/main/DelCommand.java
+++ src/main/DelCommand.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.io.IOException;
+
 public class DelCommand extends Command{
 	private String taskID;
 	private String error = "";
+	private boolean hasExecuted = false;
+	private Task task;
 	
 	public DelCommand(String args) throws Exception{
 		super(args);
@@ -33,7 +37,31 @@ public class DelCommand extends Command{
 		return checkCount();
 	}
 	
-	public boolean validTaskID(){
+	public boolean validTaskID() {
 		return checkTaskID(this.taskID);
+	}
+	
+	public String execute() {
+		task = Magical.ui.getLastTaskListDisplayed().get(taskID);
+		try {
+			Magical.storage.deleteTask(task);
+			hasExecuted = true;
+			return "task deleted";
+		} catch (IOException e) {
+			return "unable to delete task";
+		}
+	}
+	
+	public String undo() {
+		if (hasExecuted) {
+			try {
+				Magical.storage.createTask(task);
+				return "task added";
+			} catch (IOException e) {
+				return "unable to undo delete task";
+			}
+		} else {
+			return "unable to undo failed delete command";
+		}
 	}
 }
