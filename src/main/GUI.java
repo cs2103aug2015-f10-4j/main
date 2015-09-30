f9e801f9a3969bc5b138dcbb889f466576ab6f9e
yeojoey <yeojoey@gmail.com>
2015-09-30 18:48:46 +0800

    Change layout of GUI

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -20,26 +20,49 @@ public class GUI extends Application {
 	private static final int DEFAULT_WINDOW_WIDTH = 600;
 	private static final int DEFAULT_WINDOW_HEIGHT = 800;
 	
-	private TableView<Task> taskTable = new TableView<Task>();
+	
+	private TableView<Task> eventTable = new TableView<Task>();
 	private TextField commandLineTextField = new TextField();
 	private VBox vbox = new VBox(VBOX_PADDING);
 	private Scene scene = new Scene(vbox, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
-	private Label header = new Label("My Tasks");
 	
 	
-	private void makeHeader() {
+	public ArrayList<Task> tasksToDisplay = new ArrayList<Task>();
+	public ArrayList<Task> eventsToDisplay = new ArrayList<Task>();
+	
+	private void makeTestTasks() {
+		Task task1 = new Task();
+		Task task2 = new Task();
+		task1.setTitle("Get milk");
+		task1.setDueDate(new Date(115, 8, 30));
+		task2.setTitle("Do homework");
+		task2.setDueDate(new Date(115, 9, 4));
+		tasksToDisplay.add(task1);
+		tasksToDisplay.add(task2);
+		
+		Task event1 = new Task();
+		event1.setTitle("Dinner with Prof");
+		event1.setDueDate(new Date(115, 9, 3, 19, 30));
+		eventsToDisplay.add(event1);
+	}
+	
+	
+	private Label makeHeader(String text) {
+		Label header = new Label(text);
 		header.setFont(new Font("Arial", 30));
+		return header;
 	}
 	
 	private void makeVBox() {
     	vbox.setPadding(new Insets(VBOX_PADDING, VBOX_PADDING, VBOX_PADDING, VBOX_PADDING));
-    	vbox.getChildren().addAll(header, taskTable, commandLineTextField);
+    	vbox.getChildren().addAll(makeHeader("To-Do"), makeTaskTable(tasksToDisplay),
+    			makeHeader("Upcoming Events"), makeEventTable(eventsToDisplay), commandLineTextField);
 
 	}
 
-	private void makeTable() {
+	private TableView<Task> makeTaskTable(ArrayList<Task> taskList) {
 		
-		taskTable.setMinHeight(650);
+		TableView<Task> taskTable = new TableView<Task>();
 		
 		TableColumn<Task, String> taskIdCol = new TableColumn<Task, String>("ID");
 		
@@ -54,22 +77,41 @@ public class GUI extends Application {
 		colList.add(taskIdCol);
 		colList.add(taskNameCol);
 		colList.add(taskDueDateCol);
-		
-		ObservableList<Task> tasksObservableList;
+	
 		taskTable.getColumns().addAll(colList);
 		
-		Task task1 = new Task();
-		Task task2 = new Task();
-		task1.setTitle("Get milk");
-		task1.setDueDate(new Date(115, 8, 30));
-		task2.setTitle("Do homework");
-		task2.setDueDate(new Date(115, 9, 4));
-		
-		
-		ObservableList<Task> tasks = FXCollections.observableArrayList(task1, task2);
+		ObservableList<Task> tasks = FXCollections.observableArrayList(taskList);
 		taskTable.setItems(tasks);
 		taskTable.setEditable(false);
 		
+		return taskTable;
+		
+	}
+	
+	private TableView<Task> makeEventTable(ArrayList<Task> eventList) {
+		TableView<Task> eventTable = new TableView<Task>();
+		
+		TableColumn<Task, String> taskIdCol = new TableColumn<Task, String>("ID");
+		
+		TableColumn<Task, String> taskNameCol = new TableColumn<Task, String>("Event Name");
+		taskNameCol.setMinWidth(200);
+		taskNameCol.setCellValueFactory(new PropertyValueFactory<Task, String>("title"));
+		
+		TableColumn<Task, String> taskDueDateCol = new TableColumn<Task, String>("Due Date");
+		taskDueDateCol.setCellValueFactory(new PropertyValueFactory<Task, String>("dueDate"));
+		
+		ArrayList<TableColumn<Task, String>> colList = new ArrayList<TableColumn<Task, String>>();
+		colList.add(taskIdCol);
+		colList.add(taskNameCol);
+		colList.add(taskDueDateCol);
+	
+		eventTable.getColumns().addAll(colList);
+		
+		ObservableList<Task> tasks = FXCollections.observableArrayList(eventList);
+		eventTable.setItems(tasks);
+		eventTable.setEditable(false);
+		
+		return eventTable;
 	}
 	
 	private void makeCommandLine() {
@@ -78,12 +120,11 @@ public class GUI extends Application {
 	
     @Override
     public void start(Stage stage) {
+    	
+    	makeTestTasks();
 
-    	makeHeader();
     	makeVBox();
-    	makeTable();
     	makeCommandLine();
-
 
         stage.setTitle("Magical");
         stage.setScene(scene);
