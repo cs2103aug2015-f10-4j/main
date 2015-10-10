3f99d682231c37b101f3ed5c2656190f7c454088
jtwjing <jtwjing@hotmail.com>
2015-10-11 01:01:23 +0800

    add block command error responses

--- src/Commands/BlockCommand.java
+++ src/Commands/BlockCommand.java
@@ -1,6 +1,5 @@
 package Commands;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -9,49 +8,53 @@ import java.util.GregorianCalendar;
 
 public class BlockCommand extends Command{
 
-	private String[] argsArray;
 	private String type;
 	private String title;
 	private String desc;
 	private ArrayList<Date> dates;
 	private String error = "";
-	private String startD;
-	private String endD;
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-	private static final String MESSAGE_ARGUMENT_PARAMS = "\n1. block type/title/description/date 1/date 2/.../date n"
+	private Date startD;
+	private Date endD;
+	
+	private static final String MESSAGE_ERROR_PARAMS = "Number of Arguments\n"
+			+"Use Format: \n1. block type/title/description/date 1/date 2/.../date n"
 			+ "\n2. block type/title/description/start date//end date";
+	private static final String MESSAGE_ERROR_TYPE = "Type: %s (type should be event or task)\n";
+	private static final String MESSAGE_ERROR_TITLE = "No Title" + "\n";
+	private static final String MESSAGE_ERROR_DATE = "Date: %s (Date should be dd-MM-yyyy)\n";
+	private static final String MESSAGE_ERROR_SDATE = "Start date: %s (Date should be dd-MM-yyyy)\n";
+	private static final String MESSAGE_ERROR_EDATE = "End date: %s (Date should be dd-MM-yyyy)\n";
 	
 	public BlockCommand(String args) throws Exception {
 		super(args);
 		dates = new ArrayList<Date>();
 		
 		if(validNumArgs()){
-
-			System.out.println(Arrays.toString(argsArray));
 			
-			this.type = argsArray[0].trim();
-			this.title = argsArray[1].trim();
+			this.type = getType(argsArray[0].trim());
+			this.title = getTitle(argsArray[1].trim());
 			this.desc = argsArray[2].trim();
 			
-			if(!validType()){
-				error += "Type: " + type + "\n";
+			if (type == null) {
+				error += String.format(MESSAGE_ERROR_TYPE, argsArray[0].trim());
 			}
-			if(!validTitle()){
-				error += "No Title" + "\n";
+			if (title == null) {
+				error += MESSAGE_ERROR_TITLE;
 			}
-			if (!error.equals("")) {
-				throw new Exception(MESSAGE_HEADER_INVALID + error);
-			}
+//			if (!error.equals("")) {
+//				throw new Exception(MESSAGE_HEADER_INVALID + error);
+//			}
+			
 			if(args.contains("//")){
 				if(argsArray.length == 6){
-					this.startD = argsArray[3].trim();
-					this.endD = argsArray[5].trim();
+					this.startD = getDate(argsArray[3].trim());
+					this.endD = getDate(argsArray[5].trim());
 
-					if (!validStartDate()) {
-						error += "Start date: " + startD + "\n";
+					if (startD == null) {
+						error +=  String.format(MESSAGE_ERROR_SDATE, argsArray[3].trim());
 					}
-					if (!validEndDate()) {
-						error += "End date: " + endD + "\n";
+					if (endD == null) {
+						error +=  String.format(MESSAGE_ERROR_EDATE, argsArray[3].trim());
 					}
 					if (!error.equals("")) {
 						throw new Exception(MESSAGE_HEADER_INVALID + error);
@@ -60,19 +63,17 @@ public class BlockCommand extends Command{
 						// creates an array list of Dates within start and end
 						// date
 						try {
-							Date first = dateFormat.parse(startD);
-							Date last = dateFormat.parse(endD);
-							dates.add(first);
+							dates.add(startD);
 							Calendar c = new GregorianCalendar();
-							c.setTime(first);
+							c.setTime(startD);
 							c.add(Calendar.DATE, 1);
 
-							while (c.getTime().before(last)) {
+							while (c.getTime().before(endD)) {
 								Date result = c.getTime();
 								dates.add(result);
 								c.add(Calendar.DATE, 1);
 							}
-							dates.add(last);
+							dates.add(endD);
 
 						} catch (Exception e) {
 							e.printStackTrace();
@@ -85,52 +86,29 @@ public class BlockCommand extends Command{
 			} else {
 				for(int i = 3; i < argsArray.length; i++){
 					String currDate = argsArray[i].trim();
-					System.out.println(currDate);
-					boolean isValidDate = checkDate(currDate);
-					if(!isValidDate){
-						error += "Date: " + currDate + "\n";
+					Date dateToAdd = getDate(currDate);
+					if(dateToAdd == null){
+						error += String.format(MESSAGE_ERROR_DATE, currDate);
 					} else {
-						Date toAdd = dateFormat.parse(currDate);
-						dates.add(toAdd);
+						dates.add(dateToAdd);
 					}
 				}
 				if(!error.equals("")){
 					throw new Exception(MESSAGE_HEADER_INVALID + error);
 				}
 			}
-			System.out.println(dates);
 		} else {
-			error += "Number of Arguments\n";
-			throw new Exception(MESSAGE_HEADER_INVALID + error + "Use Format: " + MESSAGE_ARGUMENT_PARAMS);
-		}
-	}
-	
-	private boolean checkCount(){
-		if(this.count < 3){
-			return false;
-		} else {
-			return true;
+			error += MESSAGE_ERROR_PARAMS;
+			throw new Exception(MESSAGE_HEADER_INVALID + error);
 		}
 	}
 	
 	public boolean validNumArgs(){
-		return checkCount();
-	}
-	
-	public boolean validStartDate(){
-		return checkDate(this.startD);
-	}
-	
-	public boolean validEndDate(){
-		return checkDate(this.endD);
-	}
-	
-	public boolean validTitle(){
-		return checkTitle(this.title);
-	}
-	
-	public boolean validType(){
-		return checkType(this.type);
+		if(this.count < 4){
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	@Override
--- src/Commands/Command.java
+++ src/Commands/Command.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import main.UI;
