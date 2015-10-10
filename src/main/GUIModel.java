046075d2ea72f301d913a8709d617c643bc01d40
yeojoey <yeojoey@gmail.com>
2015-10-11 00:18:53 +0800

    Make GUIView only load from FXML

--- src/main/GUIModel.java
+++ src/main/GUIModel.java
@@ -3,26 +3,30 @@ package main;
 import java.util.*;
 
 public class GUIModel {
-	
+
 	private boolean displayNotificationBar;
 	private String notificationMessage;
 	private String errorMessage;
-	
+
 	public Task currentTask = new Task();
 	public ArrayList<Task> eventList = new ArrayList<Task>();
 	public ArrayList<Task> taskList = new ArrayList<Task>();
-	
+
 	public GUIModel() {
 	}
-	
+
 	public void setErrorMessage(String message) {
 		this.errorMessage = message;
 	}
-	
+
 	public void setTaskList(ArrayList<Task> tList) {
 		taskList = tList;
 	}
-	
-	
+
+	public void setNotificationMessage(String message) {
+		notificationMessage = message;
+	}
+
+
 
 }
--- src/main/GUIView.java
+++ src/main/GUIView.java
@@ -6,11 +6,8 @@ import javafx.application.Application;
 import javafx.collections.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.*;
-import javafx.scene.text.*;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.input.*;
 import javafx.stage.Stage;
@@ -21,207 +18,17 @@ import javafx.stage.Stage;
  * 2. DISPLAY_ONE: displays the full details of a particular task/event
  */
 public class GUIView extends Application {
-	
-	enum ViewType {
-		DISPLAY_ALL, DISPLAY_ONE
-	}
-	
-	private static final int VBOX_PADDING = 10;
-	private static final int DEFAULT_WINDOW_WIDTH = 400;
-	private static final int DEFAULT_WINDOW_HEIGHT = 600;
-	private static final String DEFAULT_FONT = "Arial";
-	
-	private static final Font FONT_HEADER = new Font(DEFAULT_FONT, 24);
-	private static final Font FONT_SUBHEADER = new Font(DEFAULT_FONT, 18);
-	private static final Font FONT_BODY = new Font(DEFAULT_FONT, 12);
-	
-	private static final Label LABEL_TODO = makeLabel("To-Do", FONT_HEADER);
-	private static final Label LABEL_EVENTS = makeLabel("Upcoming Events", FONT_HEADER);
-	private static final Label LABEL_BLOCKED = makeLabel("Blocked Dates", FONT_HEADER);
-	
-	private static final String DATE_FORMAT = "%s %s %s, %s";
-	private static final String WELCOME_MESSAGE_FORMAT = "Welcome, %s! You have %s tasks due today.";
-	
-	private TableView<Task> eventTable = new TableView<Task>();
-	
-	private static VBox vbox = new VBox(VBOX_PADDING);
-	private static Scene scene = new Scene(vbox, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
-	
-	
-	public static ArrayList<Task> taskList = new ArrayList<Task>();
-	public static ArrayList<Task> eventList = new ArrayList<Task>();
-	public static ArrayList<Task> blockedDatesList = new ArrayList<Task>();
-	public static Task currentTask = new Task();
-	public static ViewType currentViewType = ViewType.DISPLAY_ALL;
-	
-	public String userInput;
-	
-	private static Label makeLabel(String text, Font font) {
-		Label label = new Label(text);
-		label.setFont(font);
-		return label;
-	}
-	
-	private Label errorMessage = makeLabel("", FONT_BODY);
-	
-	private void makeVBox(ViewType viewType) {
-    	vbox.setPadding(new Insets(VBOX_PADDING, VBOX_PADDING, VBOX_PADDING, VBOX_PADDING));
-    	if (viewType == ViewType.DISPLAY_ALL) {
-    		vbox.getChildren().addAll(LABEL_TODO, makeTaskTable(taskList),
-        			LABEL_EVENTS, makeEventTable(eventList));
-    	} else {
-    		vbox.getChildren().addAll(makeLabel(currentTask.getTitle(), FONT_HEADER),
-    									makeLabel("Description", FONT_SUBHEADER),
-    									makeLabel(currentTask.getDescription(), FONT_BODY),
-    									makeLabel("Due Date", FONT_SUBHEADER),
-    									makeLabel(makeDateString(currentTask.getDueDate()), FONT_BODY),
-    									makeLabel("Priority", FONT_SUBHEADER),
-    									makeLabel("" + currentTask.getPriority(), FONT_BODY),
-    									makeLabel("Tags", FONT_SUBHEADER),
-    									makeLabel(makeTagString(currentTask.getTags()), FONT_BODY));
-    	}
-    	vbox.getChildren().addAll(errorMessage, makeCommandLine());
 
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Magical v0.1");
+		FXMLLoader loader = new FXMLLoader();
+		Scene scene = loader.load(getClass().getResource("FXML.fxml"));
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
-	private TableView<Task> makeTaskTable(ArrayList<Task> taskList) {
-		
-		TableView<Task> taskTable = new TableView<Task>();
-		
-		TableColumn<Task, String> taskIdCol = new TableColumn<Task, String>("ID");
-		
-		TableColumn<Task, String> taskNameCol = new TableColumn<Task, String>("Task Name");
-		taskNameCol.setMinWidth(200);
-		taskNameCol.setCellValueFactory(new PropertyValueFactory<Task, String>("title"));
-		
-		TableColumn<Task, Date> taskDueDateCol = new TableColumn<Task, Date>("Due Date");
-		taskDueDateCol.setCellValueFactory(new PropertyValueFactory<Task, Date>("dueDate"));
-		
-		TableColumn<Task, String> taskPriorityCol = new TableColumn<Task, String>("Priority");
-		taskPriorityCol.setCellValueFactory(new PropertyValueFactory<Task, String>("priority"));
-		
-		ArrayList<TableColumn<Task, ?>> colList = new ArrayList<TableColumn<Task, ?>>();
-		colList.add(taskIdCol);
-		colList.add(taskNameCol);
-		colList.add(taskDueDateCol);
-		colList.add(taskPriorityCol);
-	
-		taskTable.getColumns().addAll(colList);
-		
-		ObservableList<Task> tasks = FXCollections.observableArrayList(taskList);
-		taskTable.setItems(tasks);
-		taskTable.setEditable(false);
-		
-		return taskTable;
-		
-	}
-	
-	private TableView<Task> makeEventTable(ArrayList<Task> eventList) {
-		TableView<Task> eventTable = new TableView<Task>();
-		
-		TableColumn<Task, String> taskIdCol = new TableColumn<Task, String>("ID");
-		
-		TableColumn<Task, String> taskNameCol = new TableColumn<Task, String>("Event Name");
-		taskNameCol.setMinWidth(200);
-		taskNameCol.setCellValueFactory(new PropertyValueFactory<Task, String>("title"));
-		
-		TableColumn<Task, String> taskDueDateCol = new TableColumn<Task, String>("Due Date");
-		taskDueDateCol.setCellValueFactory(new PropertyValueFactory<Task, String>("dueDate"));
-		
-		TableColumn<Task, String> taskPriorityCol = new TableColumn<Task, String>("Priority");
-		taskPriorityCol.setCellValueFactory(new PropertyValueFactory<Task, String>("priority"));
-		
-		ArrayList<TableColumn<Task, String>> colList = new ArrayList<TableColumn<Task, String>>();
-		colList.add(taskIdCol);
-		colList.add(taskNameCol);
-		colList.add(taskDueDateCol);
-		colList.add(taskPriorityCol);
-	
-		eventTable.getColumns().addAll(colList);
-		
-		ObservableList<Task> tasks = FXCollections.observableArrayList(eventList);
-		eventTable.setItems(tasks);
-		eventTable.setEditable(false);
-		
-		return eventTable;
-	}
-	
-	private TextField makeCommandLine() {
-		final TextField commandLineTextField = new TextField();
-		commandLineTextField.setPromptText("What would you like to do?");
-		
-		commandLineTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
-		    public void handle(KeyEvent keyEvent) {
-		        if (keyEvent.getCode() == KeyCode.ENTER)  {
-		        	userInput = commandLineTextField.getText();
-		        	if (userInput == "display_one") {
-		        		currentViewType = ViewType.DISPLAY_ONE;
-		        	}
-		        	if (userInput == "display_all") {
-		        		currentViewType = ViewType.DISPLAY_ALL;
-		        	}
-		        	errorMessage.setText("You just said: " + commandLineTextField.getText());
-		        }
-		    }
-		});
-		
-		return commandLineTextField;
-	}
-	
-    public void start(Stage stage) {
-    	stage.setTitle("Magical");
-        stage.setScene(scene);
 
-    	makeVBox(currentViewType);
-    	makeCommandLine();
-    	
-    	stage.show();
-    }
-    
-    private static Stage currentStage = new Stage();
-    
-    public static void makeStage() {
-    	currentStage.setTitle("Magical");
-    	currentStage.setScene(scene);
-    }
-    
-    private static final String[] DAYS_ARRAY = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    private static final String[] MONTHS_ARRAY = {"January", "February", "March", "April", "May", "June", "July",
-    										"August", "Septembers", "October", "November", "December"};
-    
-    public static String makeDateString(Date date) {
-    	return String.format(DATE_FORMAT, date.getDate()+1, MONTHS_ARRAY[date.getMonth()], date.getYear()+1900, DAYS_ARRAY[date.getDay()]);
-    }
-    
-    public static String makeTagString(Set<String> tagSet) {
-    	Iterator<String> iterator = tagSet.iterator();
-    	String result = "";
-    	for (int i = 0; i < tagSet.size()-1; i ++) {
-    		result += iterator.next() + ", ";
-    	}
-    	result += iterator.next();
-    	return result;
-    }
-    
-    
     public static void main(String[] args) {
-    	
-    	HashSet<String> tagSet = new HashSet<String>();
-    	tagSet.add("tag1");
-    	tagSet.add("tag2");
-    	
-    	Task task1 = new Task();
-    	task1.setTitle("Test task");
-    	task1.setDescription("Task description goes here!");
-    	task1.setDueDate(new Date(114, 12, 11));
-    	task1.setPriority(2);
-    	task1.setTags(tagSet);
-    	
-    	taskList.add(task1);
-    	currentTask = task1;
-    	
-    	eventList.add(task1);
-    	
     	launch(args);
 
     }
