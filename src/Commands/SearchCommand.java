b9f1a19ceae5f58976134e8836e1ea1e42bfef32
jtwjing <jtwjing@hotmail.com>
2015-10-20 11:22:14 +0800

    modified "show" to work properly

--- src/Commands/SearchCommand.java
+++ src/Commands/SearchCommand.java
@@ -18,17 +18,19 @@ public class SearchCommand extends Command {
 		super(args);
 		
 		if (validNumArgs()) {
-			this.query = argsArray[0].trim();
-
-			this.type = argsArray[1].trim();
-			this.type = type.equals("") ? "" : getType(type);
-
-			if (type == null) {
-				error += "Type: " + type + "\n";
-			}
-
-			if (!error.equals("")) {
-				throw new Exception(MESSAGE_HEADER_INVALID + error);
+			if(count == 2){
+				this.query = argsArray[0].trim();
+				
+				this.type = argsArray[1].trim();
+				this.type = type.equals("") ? "" : getType(type);
+	
+				if (type == null) {
+					error += "Type: " + type + "\n";
+				}
+	
+				if (!error.equals("")) {
+					throw new Exception(MESSAGE_HEADER_INVALID + error);
+				}
 			}
 		} else {
 			error += "Number of Arguments\n";
@@ -46,13 +48,17 @@ public class SearchCommand extends Command {
 	
 	public String execute() {
 		ArrayList<Task> results = Magical.storage.getTasks();
-		ArrayList<Task> filteredResults = new ArrayList<Task>();
-		for (Task t : results) {
-			if ((t.getTitle().contains(query) || t.getDescription().contains(query)) && t.getType().contains(type)) {
-				filteredResults.add(t);
+		if(count == 2){
+			ArrayList<Task> filteredResults = new ArrayList<Task>();
+			for (Task t : results) {
+				if ((t.getTitle().contains(query) || t.getDescription().contains(query)) && t.getType().contains(type)) {
+					filteredResults.add(t);
+				}
 			}
+			UI.displayTaskList("Search results", filteredResults);
+		} else {
+			UI.displayTaskList("Search results", results);
 		}
-		UI.displayTaskList("Search results", filteredResults);
 		return null;
 	}
 	
