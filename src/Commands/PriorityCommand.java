225a26e4b5772c1877d0e155bc225d20b6fb69d2
jtwjing <jtwjing@hotmail.com>
2015-10-27 22:36:29 +0800

    update PriorityCommand constructor

--- src/Commands/PriorityCommand.java
+++ src/Commands/PriorityCommand.java
@@ -10,22 +10,28 @@ public class PriorityCommand extends Command{
 
 	private Task task;
 	private int priority;
-	private String error = "";
+	private String error = STRING_EMPTY;
 	private Task prevTask;
 	
-	private static final String MESSAGE_ARGUMENT_PARAMS = "set task id/priority";
+	private static final String MESSAGE_ARGUMENT_PARAMS = "set <task id> <priority>";
+	private static final String MESSAGE_INVALID_ID = "Task ID: %s\n";
+	private static final String MESSAGE_INVALID_PRIORITY = "Priority: %s\n";
 	
 	public PriorityCommand(String args) throws Exception{
 		super(args);
 		
+		this.argsArray = args.split(" ", 2);
+		this.count = argsArray.length;
+		
 		if(validNumArgs()){
 			task = getTaskByID(argsArray[0].trim());
 			priority = getPriority(argsArray[1].trim());
+			
 			if(task == null){
-				error += "Task ID: " + argsArray[0] + "\n";
+				error += String.format(MESSAGE_INVALID_ID, argsArray[0].trim());
 			}
 			if(priority == -1){
-				error += "Priority: " + priority + "\n";
+				error += String.format(MESSAGE_INVALID_PRIORITY, argsArray[1].trim());
 			}
 			if (!error.equals("")) {
 				throw new Exception(MESSAGE_HEADER_INVALID + error);
