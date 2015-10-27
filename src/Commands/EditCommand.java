a73ce3f8aeb1c2a8caed5f92a4e570e231e26d89
jtwjing <jtwjing@hotmail.com>
2015-10-27 22:26:40 +0800

    update EditCommand constructor

--- src/Commands/EditCommand.java
+++ src/Commands/EditCommand.java
@@ -3,6 +3,7 @@ package Commands;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 import main.Magical;
 import main.RecurrencePeriod;
@@ -13,14 +14,27 @@ public class EditCommand extends Command{
 
 	private String field;
 	private String value;
-	private String error = "";
+	private String error = STRING_EMPTY;
 	private Task task;
 	private Task prevTask;
 	
-	private static final String MESSAGE_ARGUMENT_PARAMS = "edit task_id/field/value";
+	private static final String MESSAGE_ARGUMENT_PARAMS = "edit <task_id> <field> <value>";
+	private static final String MESSAGE_INVALID_ID = "Task ID: %s\n";
+	private static final String MESSAGE_INVALID_FIELD = "Field: %s\n";
+	private static final String MESSAGE_ERROR_TITLE = "No Title\n";
+	private static final String MESSAGE_ERROR_DATE = "Date: %s (Date should be dd-MM-yyyy)\n";
+	private static final String MESSAGE_ERROR_START = "Start time: %s (Time should be in 24hrs format)\n";
+	private static final String MESSAGE_ERROR_END = "End time: %s (Time should be in 24hrs format)\n";
+	private static final String MESSAGE_ERROR_RECURRENCE = "Recurrence: %s"
+			+ "\n(Recurrence should be daily, weekly, monthly, yearly or left empty\n";
 	
 	public EditCommand(String args) throws Exception {
 		super(args);
+		
+		this.argsArray = args.split(" ", 3);
+		this.count = argsArray.length;
+		
+		System.out.println(Arrays.toString(argsArray));
 		
 		if(validNumArgs()){
 			this.task = getTaskByID(argsArray[0].trim());
@@ -28,27 +42,26 @@ public class EditCommand extends Command{
 			this.value = argsArray[2].trim();
 			
 			if (task == null) {
-				error += "Task ID: " + argsArray[0] + "\n";
+				error += String.format(MESSAGE_INVALID_ID, argsArray[0].trim());
 			}
+			
 			if (field.equalsIgnoreCase("title") && (getTitle(value) == null)) {
-				error += "No Title" + "\n";
+				error += MESSAGE_ERROR_TITLE;
+			} else if (field.equalsIgnoreCase("date") && (getDate(value) == null)) {
+				error +=  String.format(MESSAGE_ERROR_DATE, value);
+			} else if (field.equalsIgnoreCase("start time") && (getTime(value) == -1)) {
+				//NOTE: Shouldn't be able to edit this for tasks
+				error += String.format(MESSAGE_ERROR_START, value);
+			} else if (field.equalsIgnoreCase("end time") && (getTime(value) == -1)) {
+				//NOTE: Shouldn't be able to edit this for floating tasks
+				error += String.format(MESSAGE_ERROR_END, value);
+			} else if (field.equalsIgnoreCase("recurrence") && (getRecurrence(value) == null)) {
+				error += String.format(MESSAGE_ERROR_RECURRENCE, value);
+			} else {
+				error += String.format(MESSAGE_INVALID_FIELD, field);
 			}
-			else if (field.equalsIgnoreCase("due date") && (getDate(value) == null)) {
-				error += "Due date: " + value + "\n";
-			}
-			else if (field.equalsIgnoreCase("start time") && (getTime(value) == -1)) {
-				error += "Start time: " + value + "\n";
-			}
-			else if (field.equalsIgnoreCase("end time") && (getTime(value) == -1)) {
-				error += "End time: " + value + "\n";
-			}
-			else if (field.equalsIgnoreCase("recurrence") && (getRecurrence(value) == null)) {
-				error += "Recurrence: " + value + "\n";
-			}
-//			else {
-//				error += "Invalid field.\n";
-//			}
-			if (!error.equals("")) {
+			
+			if (!error.equals(STRING_EMPTY)) {
 				throw new Exception(MESSAGE_HEADER_INVALID + error);
 			}
 		} else {
@@ -78,7 +91,7 @@ public class EditCommand extends Command{
 		case "title":
 			task.setTitle(value);
 			break;
-		case "due date":
+		case "date":
 			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 			try {
 				task.setDueDate(dateFormat.parse(value));
@@ -107,5 +120,9 @@ public class EditCommand extends Command{
 		}
 		
 		return "Task edited.";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		EditCommand e = new EditCommand("t1 date aihfaf");
 	}
 }
