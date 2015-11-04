b8de923dbc64095a2e358e9207941156f1407295
yeojoey <yeojoey@gmail.com>
2015-11-05 00:54:08 +0800

    Change CustomDate year format

--- src/main/CustomDate.java
+++ src/main/CustomDate.java
@@ -5,15 +5,22 @@ import java.util.Date;
 
 public class CustomDate extends Date {
 
-	private static final String DATE_FORMAT = "%i %s %i, %s";
+	private static final String DATE_FORMAT = "%i %s %s, %s";
 
-	private static String[] dayArray = {"Sunday", "Monday", "Tuesday", "Wednesday",
-			"Thursday", "Friday", "Saturday"};
+	private static String[] dayArray = {"Sun", "Mon", "Tues", "Wed",
+			"Thur", "Fri", "Sat"};
 	private static String[] monthArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
 			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 	public CustomDate(String args) {
 		super(args);
+	}
+
+	public String getYearString() {
+		Calendar c = Calendar.getInstance();
+		c.setTime(this);
+		String year = Integer.toString(c.get(Calendar.YEAR));
+		return "'" + year.substring(2);
 	}
 
 	@Override
@@ -22,6 +29,6 @@ public class CustomDate extends Date {
 		c.setTime(this);
 		String day = dayArray[c.get(Calendar.DAY_OF_WEEK)];
 		String month = monthArray[c.get(Calendar.MONTH)];
-		return String.format(DATE_FORMAT, c.get(Calendar.DAY_OF_MONTH), month, c.get(Calendar.YEAR), day);
+		return String.format(DATE_FORMAT, c.get(Calendar.DAY_OF_MONTH), month, getYearString(), day);
 	}
 }
