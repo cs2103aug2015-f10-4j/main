7b5b32430aea49d50aa7486b428d7b223b6cf2ee
jtwjing <jtwjing@hotmail.com>
2015-11-05 02:15:03 +0800

    create PathCommand skeleton

---
+++ src/Commands/PathCommand.java
@@ -0,0 +1,50 @@
+package Commands;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PathCommand extends Command {
+
+	private String location;
+	
+	private static final String MESSAGE_INVALID_PARAMS = "Number of Arguments\n"
+			+ "Use Format: path <location>";
+	
+	public PathCommand(String args) throws Exception {
+		super(args);
+
+		this.argsArray = new ArrayList<String>(Arrays.asList(args.split(" ")));
+		this.count = argsArray.size();
+		
+		if(validNumArgs()){
+			this.location = argsArray.get(1);
+			File file = new File(location);
+			
+			//this assumes that directory exists
+			if(!file.isDirectory()){
+				//wanted to throw error
+			}
+		} else {
+			error += MESSAGE_INVALID_PARAMS;
+			throw new Exception(MESSAGE_HEADER_INVALID + error);
+		}
+		
+	}
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validNumArgs() {
+		if(count != 1){
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
