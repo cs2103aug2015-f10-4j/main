4fad43ee860fed33577f7406d7b9d816519e09e5
Varun Patro <varun.kumar.patro@gmail.com>
2015-11-06 00:03:25 +0800

    Clean and document Path.

--- src/Commands/PathCommand.java
+++ src/Commands/PathCommand.java
@@ -1,18 +1,13 @@
 package Commands;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import main.Magical;
 
 public class PathCommand extends Command {
 
 	private String location;
-	
-	private static final String MESSAGE_INVALID_PARAMS = "Number of Arguments\n"
-			+ "Use Format: path <location>";
-	
+
 	public PathCommand(String args) throws Exception {
 		super(args);
 
@@ -20,9 +15,17 @@ public class PathCommand extends Command {
 		argsArray.add(args);
 		this.count = argsArray.size();
 		this.location = args;
-		
+
 	}
 
+	/**
+	 * This method executes the path command. Which changes the file path of the
+	 * database file.
+	 * 
+	 * @param None
+	 * 
+	 * @return message to show user
+	 */
 	@Override
 	public String execute() throws Exception {
 		Magical.storage.changeFilePath(location);
