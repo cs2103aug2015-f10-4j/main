8e686d356a3de0f939a54d2384b0f67ef8c0ed59
yeojoey <yeojoey@gmail.com>
2015-11-06 01:29:18 +0800

    Add documentation

--- src/gui/GUIModel.java
+++ src/gui/GUIModel.java
@@ -16,8 +16,6 @@ import main.Task;
 
 public class GUIModel {
 
-	private static final String ENDL = System.getProperty("line.separator");
-
 	public static boolean showHelpWindow = false;
 
 	public static String currentTab = "tasks";
@@ -26,6 +24,11 @@ public class GUIModel {
 	public static ObservableList<Task> taskDoneList;
 	public static ObservableList<Task> eventList;
 	public static ObservableList<Task> eventDoneList;
+
+	/**
+	 * This method sets the current tab in the model.
+	 * @param type - "tasks" or "events"
+	 */
 
 	public static void setCurrentTab(String type) {
 		if (type == "tasks") {
@@ -36,40 +39,83 @@ public class GUIModel {
 		}
 	}
 
+	/**
+	 * Returns the current tab in the model.
+	 * @return String - "events" or "tasks"
+	 */
 	public static String getCurrentTab() {
 		return currentTab;
 	}
 
+	/**
+	 * Return the model's task list.
+	 * @return ObservableList<Task>
+	 */
 	public static ObservableList<Task> getTaskList() {
 		return taskList;
 	}
 
+	/**
+	 * Return the model's done task list.
+	 * @return ObservableList<Task>
+	 */
 	public static ObservableList<Task> getTaskDoneList() {
 		return taskDoneList;
 	}
+
+	/**
+	 * Return the model's event list.
+	 * @return ObservableList<Task>
+	 */
 	public static ObservableList<Task> getEventList() {
 		return eventList;
 	}
 
+	/**
+	 * Return the model's done event list.
+	 * @return ObservableList<Task>
+	 */
 	public static ObservableList<Task> getEventDoneList() {
 		return eventDoneList;
 	}
 
+	/**
+	 * Sets the model's task list.
+	 * @param newTaskList - replaces current task list
+	 */
 	public static void setTaskList(ArrayList<Task> newTaskList) {
 		taskList = makeObservable(newTaskList);
 	}
 
+	/**
+	 * Sets the model's done task list.
+	 * @param newTaskList - replaces current done task list
+	 */
 	public static void setTaskDoneList(ArrayList<Task> newTaskDoneList) {
 		taskDoneList = makeObservable(newTaskDoneList);
 	}
 
+	/**
+	 * Sets the model's event list.
+	 * @param newTaskList - replaces current event list
+	 */
 	public static void setEventList(ArrayList<Task> newEventList) {
 		eventList = makeObservable(newEventList);
 	}
 
+	/**
+	 * Sets the model's done event list.
+	 * @param newTaskList - replaces current done event list
+	 */
 	public static void setEventDoneList(ArrayList<Task> newEventDoneList) {
 		eventDoneList = makeObservable(newEventDoneList);
 	}
+
+	/**
+	 * This method initializes GUIModel by converting tasks lists from Storage
+	 * into ObservableLists usable by the controller.
+	 * @return Nothing
+	 */
 
 	public static void init() {
 		taskList = makeObservable(main.Magical.getStorage().getList(main.Storage.TASKS_INDEX));
@@ -78,21 +124,14 @@ public class GUIModel {
 		eventDoneList = makeObservable(main.Magical.getStorage().getList(main.Storage.EVENTS_DONE_INDEX));
 	}
 
+	/**
+	 * This method converts an arraylist of tasks into an ObservableList.
+	 * @param arrayList - list to convert
+	 * @return ObservableList
+	 */
 	private static ObservableList<Task> makeObservable(ArrayList<Task> arrayList) {
 		return FXCollections.observableArrayList(arrayList);
 	}
 
-//	public static void main(String[] args) {
-//		ArrayList<Task> testList = new ArrayList<Task>();
-//		Task t = new Task();
-//		t.setTitle("TEST EVENT");
-//		t.setStartTime(1600);
-//		t.setEndTime(1800);
-//		testList.add(t);
-//		setEventList(testList);
-//		System.out.println(getEventList());
-//		System.out.println(getEventList().get(0).getStartTime());
-//		System.out.println(getEventList().get(0).getEndTime());
-//	}
 
 }
