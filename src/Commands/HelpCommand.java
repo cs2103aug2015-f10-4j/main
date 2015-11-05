8f5f1cb13edf275639a24abdea5e2952d27cd544
Varun Patro <varun.kumar.patro@gmail.com>
2015-11-06 00:03:20 +0800

    Clean and document Help.

--- src/Commands/HelpCommand.java
+++ src/Commands/HelpCommand.java
@@ -1,13 +1,20 @@
 package Commands;
 
-import gui.GUIView;
-
 public class HelpCommand extends Command {
 
 	public HelpCommand(String args) throws Exception {
 		super(args);
 	}
 
+	/**
+	 * This method executes the help command. Which triggers the GUI to launch
+	 * the help window.
+	 * 
+	 * @param None
+	 *            .
+	 * @return None
+	 */
+	@Override
 	public String execute() throws Exception {
 		gui.GUIModel.showHelpWindow = true;
 		return null;
