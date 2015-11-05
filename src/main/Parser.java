ad38c068931bebc8582708a9baa42c613e797f1d
jtwjing <jtwjing@hotmail.com>
2015-11-05 22:58:17 +0800

    comments for parser

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -3,6 +3,8 @@ package main;
 import Commands.*;
 
 public class Parser {
+	
+	/** List of Command types */
 	private static final String CMD_ADD = "add";
 	private static final String CMD_DELETE = "delete";
 	private static final String CMD_UNDO = "undo";
@@ -21,8 +23,20 @@ public class Parser {
 	private static final String CMD_PATH = "path";
 	private static final String CMD_SORT = "sort";
 	
-	static final String MESSAGE_INVALID_COMMAND = "Invalid command";
+	/** Error Messages*/
+	private static final String MESSAGE_INVALID_COMMAND = "Invalid command";
+	private static final String MESSAGE_INVALID_INPUT = "Must enter a command when calling execute";
 	
+	/**
+	 * Method: parse
+	 * Description: Takes in an input from the user and returns the correct command object
+	 * for executing what the user wants. Calls the inputValidation method. Exception is 
+	 * thrown if no input is given.
+	 * 
+	 * @param userInput
+	 * @return Command object to use for executing
+	 * @throws Exception
+	 */
 	public static Command parse(String userInput) throws Exception{
 		if(userInput != null){
 			String trimInput = userInput.trim();
@@ -30,15 +44,31 @@ public class Parser {
 			String args = splitArgs(trimInput);
 			return inputValidation(command, args);
 		} else {
-			throw new Exception("Must enter a command when calling execute");
+			throw new Exception(MESSAGE_INVALID_INPUT);
 		}
 	}
 	
+	/**
+	 * Method: splitCommand
+	 * Description: splits a given string of user input and returns the first argument,
+	 * which should be the command
+	 * 
+	 * @param userInput
+	 * @return String stating the command given by user
+	 */
 	private static String splitCommand(String userInput) {
 		String[] toSplit = userInput.split(" ", 2);
 		return toSplit[0].toLowerCase().trim();
 	}
 
+	/**
+	 * Method: splitArgs
+	 * Description: splits a given string of user input and returns the words after the 
+	 * first word of the input, which are the arguments of the command
+	 * 
+	 * @param userInput
+	 * @return String containing the arguments of the command
+	 */
 	private static String splitArgs(String userInput) {
 		String[] toSplit = userInput.split(" ", 2);
 		if(toSplit.length > 1){
@@ -48,7 +78,18 @@ public class Parser {
 		}
 	}
 	
-	//checks if command and args are valid
+	/**
+	 * Method: inputValidation
+	 * Description: checks if the command is valid and returns the correct Command subclass
+	 * of the user's command. Passes the arguments to the Command subclass, which will be 
+	 * checked for validity within the constructor. Exception thrown if a command is not 
+	 * valid 
+	 * 
+	 * @param command
+	 * @param args
+	 * @return Command subclass object according to the user command
+	 * @throws Exception
+	 */
 	private static Command inputValidation(String command, String args) throws Exception{
 		args = args == null ? "" : args;
 		switch (command) {
