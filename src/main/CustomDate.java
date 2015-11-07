0f162424e42947dfdf378b8a25d23fe8b58a94e5
junewonewlim <junelym1@gmail.com>
2015-11-07 22:52:07 +0800

    Override hashcode() and equals() to compare CustomDate objects

--- src/main/CustomDate.java
+++ src/main/CustomDate.java
@@ -31,6 +31,36 @@ public class CustomDate implements Comparable {
 		return String.format(DATE_FORMAT, getTime(), getDay(), month,
 				getYear(), day);
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof CustomDate)) {
+			return false;
+		}
+		CustomDate other = (CustomDate) obj;
+		if (date == null) {
+			if (other.date != null) {
+				return false;
+			}
+		} else if (!date.equals(other.date)) {
+			return false;
+		}
+		return true;
+	}
 
 	public String getDateString() {
 		String dateString = getDay() + "/" + getMonth() + "/" + getYear();
