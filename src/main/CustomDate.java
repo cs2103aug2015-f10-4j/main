81e478f83a74c8776350a5bd80b116c9ca2179d6
jtwjing <jtwjing@hotmail.com>
2015-11-09 21:17:21 +0800

    fix customdate tostring

--- src/main/CustomDate.java
+++ src/main/CustomDate.java
@@ -212,7 +212,8 @@ public class CustomDate implements Comparable<CustomDate> {
 		String day = dayArray[cal.get(Calendar.DAY_OF_WEEK) - 1];
 		String month = monthArray[getMonth() - 1];
 		int time = getTime();
-		String timeString = String.format("%02d.%02d", time/200, time%100);
+		int hour = time/100 - 12 <= 0 ? time/100 : time/100 - 12;
+		String timeString = String.format("%02d.%02d", hour, time%100);
 		if (time / 100 < 12) {
 			timeString += " am";
 		} else {
