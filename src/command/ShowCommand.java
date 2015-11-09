7f15fd525a4598f343fd0be0904655486b114d41
Varun Patro <varun.kumar.patro@gmail.com>
2015-11-09 17:38:48 +0800

    Convert all tags to lower case before showing.

--- src/command/ShowCommand.java
+++ src/command/ShowCommand.java
@@ -117,7 +117,10 @@ public class ShowCommand extends Command {
 	 */
 	private ArrayList<Item> filterList(ArrayList<Item> itemList) {
 		ArrayList<Item> filteredItemList = new ArrayList<Item>();
-		Set<String> queryTags = new HashSet<String>(tags);
+		Set<String> queryTags = new HashSet<String>(tags); 
+		for (String tag : tags) {
+			queryTags.add(tag.toLowerCase());
+		}
 		for (Item i : itemList) {
 			if (i.getTags().containsAll(queryTags)) {
 				filteredItemList.add(i);
