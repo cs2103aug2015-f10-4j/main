e58dfa1d49237c8a9265767f9ad59a28394494a3
yeojoey <yeojoey@gmail.com>
2015-11-09 21:15:25 +0800

    Throw exception

--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -446,7 +446,7 @@ public class GUIController {
 	 * the cell depending on priority.
 	 * @return TableCell<Item, CustomDate> with a colored row depending on date
 	 */
-	private TableCell<Item, String> makePriorityCellFactory() {
+	private TableCell<Item, String> makePriorityCellFactory(){
 		return new TableCell<Item, String>() {
 			@Override
 			protected void updateItem(String item, boolean empty) {
@@ -457,16 +457,7 @@ public class GUIController {
 				}
 				else {
 					setText(item);
-					if (!getTableRow().getStyle().isEmpty()) {
-						return;
-					} else if (item.equals(PRIORITY_HIGH)) {
-						setStyle("-fx-background-color: " + PRIORITY_HIGH_COLOR);
-					} else if (item.equals(PRIORITY_MEDIUM)) {
-						setStyle("-fx-background-color: " + PRIORITY_MEDIUM_COLOR);
-					} else if (item.equals(PRIORITY_LOW)) {
-						setStyle("-fx-background-color: " + PRIORITY_LOW_COLOR);
-					} else {
-					}
+
 					return;
 				}
 			}
